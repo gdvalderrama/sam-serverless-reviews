30fe4f5630ae48f54d529bd6ac6d753e05e34b26
Guiomar Valderrama <guival4@gmail.com>
2017-08-29 18:40:10 +0200

    sam template that defines CRUD using api gateway, lambda functions, and dynamodb; lambda functions to connect to dynamodb and return

---
+++ lambda_reviews.py
@@ -0,0 +1,38 @@
+import os
+import logging
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb_client = boto3.client("dynamodb")
+table_name = os.environ["TABLE_NAME"]
+
+logger.info("Using table {}".format(table_name))
+
+
+def create_response(status_code, body):
+    response = {
+        "statusCode": status_code,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(body) or ""
+    }
+    logger.info("Created response: {}".format(response))
+    return response
+
+
+def handler_get(event, context):
+    logger.info("Received event {}".format(event))
+
+    try:
+        result = dynamodb_client.get_item(
+            TableName=table_name,
+            Key={"id": {"S": event["pathParameters"]["reviewId"]}}
+        )
+        logger.info("Get Item result: {}".format(result))
+        return create_response(200, result["Item"])
+    except ClientError as e:
+        logger.info("Get Item failed: {}".format(e))
+        return create_response(500, e.response["Error"]["Message"])
